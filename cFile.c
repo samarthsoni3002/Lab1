8b2e6b29424a5eb3609dd67496073e3630b10d6e
Vaibhavbansal15 <106023582+Vaibhavbansal15@users.noreply.github.com>
2023-08-01 11:25:07 +0530

    Update cFile.c
    Added a Multiplication feature in the present code.

--- cFile.c
+++ cFile.c
@@ -1,25 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int main(void) {
 
-int main(void){
+  int a;
+  int b;
 
-    int a;
-    int b;
+  printf("Enter a number:- ");
+  scanf("%d", &a);
 
-    printf("Enter a number:- ");
-    scanf("%d",&a);
+  printf("Enter another number:- ");
+  scanf("%d", &b);
 
-    printf("Enter another number:- ");
-    scanf("%d",&b);
+  int c;
+  c = a + b;
+  printf("\n The sum of %d and %d is %d", a, b, c);
 
-
-    int c;
-
-    c = a+b;
-
-    printf("The sum of %d and %d is %d",a,b,c);
-
-
-
-
+  int product;
+  product = a * b;
+  printf("\n The Product of %d and %d is: %d", a, b, product);
 }
