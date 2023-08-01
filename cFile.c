21413b8ac0cef2335ac8216bbc2755e2ae6545cb
samarthsoni3002 <88267506+samarthsoni3002@users.noreply.github.com>
2023-08-01 10:22:07 +0530

    Add files via upload

---
+++ cFile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main(void){
+
+    int a;
+    int b;
+
+    printf("Enter a number:- ");
+    scanf("%d",&a);
+
+    printf("Enter another number:- ");
+    scanf("%d",&b);
+
+
+    int c;
+
+    c = a+b;
+
+    printf("The sum of %d and %d is %d",a,b,c);
+
+
+
+
+}
